6ec44dd804a88bd62ee28b846c94019f43ceaff7
Antti Poikela <antti.poikela@iki.fi>
2016-02-17 00:03:51 +0100

    Inherit Beer from reark OverwritablePojo

--- app/src/main/java/quickbeer/android/next/pojo/Beer.java
+++ app/src/main/java/quickbeer/android/next/pojo/Beer.java
@@ -1,21 +1,15 @@
 package quickbeer.android.next.pojo;
 
-import android.net.Uri;
 import android.support.annotation.NonNull;
 
 import com.google.gson.annotations.SerializedName;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
-import io.reark.reark.utils.Log;
+import io.reark.reark.pojo.OverwritablePojo;
 
 /**
  * Created by antti on 17.10.2015.
  */
-public class Beer {
-    private static final String TAG = Beer.class.getSimpleName();
-
+public class Beer extends OverwritablePojo<Beer> {
     @SerializedName("BeerID")
     private int id;
 
@@ -61,6 +55,12 @@ public class Beer {
     @SerializedName("BrewerCountryId")
     private int countryId;
 
+    @NonNull
+    @Override
+    protected Class<Beer> getTypeParameterClass() {
+        return Beer.class;
+    }
+
     public int getId() {
         return id;
     }
@@ -94,65 +94,6 @@ public class Beer {
 
     public String getImageUri() {
         return String.format("https://res.cloudinary.com/ratebeer/image/upload/w_250,c_limit/beer_%d.jpg", getId());
-    }
-
-    public Beer overwrite(Beer other) {
-        if (id != other.id) {
-            return other;
-        } else if (equals(other)) {
-            return this;
-        }
-
-        for (Field field : Beer.class.getDeclaredFields()) {
-            final int modifiers = field.getModifiers();
-            if (Modifier.isFinal(modifiers) || Modifier.isStatic(modifiers)) {
-                continue;
-            }
-
-            try {
-                if (!isEmpty(field, other)) {
-                    field.set(this, field.get(other));
-                }
-            } catch (IllegalAccessException e) {
-                Log.e(TAG, "Failed set at " + field.getName(), e);
-            }
-        }
-
-        return this;
-    }
-
-    private static boolean isEmpty(Field field, Beer beer) {
-        try {
-            Object value = field.get(beer);
-            if (value == null) {
-                return true;
-            } if (value instanceof String) {
-                return isEmpty((String) value);
-            } else if (value instanceof Integer) {
-                return isEmpty((Integer) value);
-            } else if (value instanceof Float) {
-                return isEmpty((Float) value);
-            } else if (value instanceof Boolean) {
-                return false;
-            }
-        } catch (IllegalAccessException e) {
-            Log.e(TAG, "Failed get at " + field.getName(), e);
-        }
-
-        Log.w(TAG, "Unknown field type: " + field.getName());
-        return true;
-    }
-
-    private static boolean isEmpty(String value) {
-        return value == null || value.isEmpty();
-    }
-
-    private static boolean isEmpty(float value) {
-        return value <= 0.0f;
-    }
-
-    private static boolean isEmpty(int value) {
-        return value <= 0;
     }
 
     public boolean hasDetails() {
