eb3c2bd6c1af5e464bc4e53bc5a749a2426cc4d1
Antti Poikela <antti.poikela@iki.fi>
2017-08-01 23:39:25 +0200

    Update dependencies

--- app/src/main/java/quickbeer/android/utils/glide/SvgModule.java
+++ app/src/main/java/quickbeer/android/utils/glide/SvgModule.java
@@ -3,6 +3,7 @@ package quickbeer.android.utils.glide;
 import android.content.Context;
 import android.graphics.drawable.PictureDrawable;
 
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
@@ -16,7 +17,7 @@ import java.io.InputStream;
 @GlideModule
 public class SvgModule extends AppGlideModule {
     @Override
-    public void registerComponents(Context context, Registry registry) {
+    public void registerComponents(Context context, Glide glide, Registry registry) {
         registry.register(SVG.class, PictureDrawable.class, new SvgDrawableTranscoder())
                 .append(InputStream.class, SVG.class, new SvgDecoder());
     }
