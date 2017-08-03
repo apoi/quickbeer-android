d6edb94c6dd36ee69820f3c5c7380d3b3ddbc91d
Antti Poikela <antti.poikela@iki.fi>
2017-08-04 00:43:41 +0200

    Add missing license headers for Glide

--- app/src/main/java/quickbeer/android/utils/glide/SvgSoftwareLayerSetter.java
+++ app/src/main/java/quickbeer/android/utils/glide/SvgSoftwareLayerSetter.java
@@ -1,3 +1,30 @@
+/**
+ * Copyright 2014 Google, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *          conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *          of conditions and the following disclaimer in the documentation and/or other materials
+ *          provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY GOOGLE, INC. ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GOOGLE, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Google, Inc.
+ */
 package quickbeer.android.utils.glide;
 
 import android.graphics.drawable.PictureDrawable;
@@ -18,16 +45,14 @@ import com.bumptech.glide.request.target.ViewTarget;
 public class SvgSoftwareLayerSetter implements RequestListener<PictureDrawable> {
 
     @Override
-    public boolean onLoadFailed(GlideException e, Object model, Target<PictureDrawable> target,
-                                boolean isFirstResource) {
+    public boolean onLoadFailed(GlideException e, Object model, Target<PictureDrawable> target, boolean isFirstResource) {
         ImageView view = ((ViewTarget<ImageView, ?>) target).getView();
         view.setLayerType(View.LAYER_TYPE_NONE, null);
         return false;
     }
 
     @Override
-    public boolean onResourceReady(PictureDrawable resource, Object model,
-                                   Target<PictureDrawable> target, DataSource dataSource, boolean isFirstResource) {
+    public boolean onResourceReady(PictureDrawable resource, Object model, Target<PictureDrawable> target, DataSource dataSource, boolean isFirstResource) {
         ImageView view = ((ViewTarget<ImageView, ?>) target).getView();
         view.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         return false;
