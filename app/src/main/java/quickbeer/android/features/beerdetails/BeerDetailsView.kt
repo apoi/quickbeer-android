76c0285603cf05e6edfbd21bb04c64f3b0dd0267
Antti Poikela <antti.poikela@iki.fi>
2017-10-18 16:42:35 +0200

    Convert beer and brewer views to Kotlin

---
+++ app/src/main/java/quickbeer/android/features/beerdetails/BeerDetailsView.kt
@@ -0,0 +1,214 @@
+/**
+ * This file is part of QuickBeer.
+ * Copyright (C) 2016 Antti Poikela <antti.poikela></antti.poikela>@iki.fi>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http:></http:>//www.gnu.org/licenses/>.
+ */
+package quickbeer.android.features.beerdetails
+
+import android.content.Context
+import android.content.Intent
+import android.support.annotation.StringRes
+import android.support.v4.widget.NestedScrollView
+import android.support.v7.app.AlertDialog
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RatingBar
+import android.widget.Toast
+import kotlinx.android.synthetic.main.beer_details_fragment_details.view.*
+import polanski.option.Option
+import polanski.option.Option.ofObj
+import quickbeer.android.Constants
+import quickbeer.android.R
+import quickbeer.android.core.activity.InjectingDrawerActivity
+import quickbeer.android.data.pojos.Beer
+import quickbeer.android.data.pojos.Brewer
+import quickbeer.android.data.pojos.User
+import quickbeer.android.data.stores.BeerStyleStore
+import quickbeer.android.data.stores.CountryStore
+import quickbeer.android.features.brewerdetails.BrewerDetailsActivity
+import quickbeer.android.features.countrydetails.CountryDetailsActivity
+import quickbeer.android.features.profile.ProfileActivity
+import quickbeer.android.features.styledetails.StyleDetailsActivity
+import quickbeer.android.providers.NavigationProvider
+import quickbeer.android.providers.ResourceProvider
+import quickbeer.android.providers.ToastProvider
+import quickbeer.android.utils.DateUtils
+import quickbeer.android.utils.StringUtils
+import timber.log.Timber
+import java.util.*
+import javax.inject.Inject
+
+/**
+ * View holder for all the beer details
+ */
+class BeerDetailsView(context: Context, attrs: AttributeSet) : NestedScrollView(context, attrs) {
+
+    @Inject
+    internal lateinit var countryStore: CountryStore
+
+    @Inject
+    internal lateinit var beerStyleStore: BeerStyleStore
+
+    @Inject
+    internal lateinit var resourceProvider: ResourceProvider
+
+    @Inject
+    internal lateinit var toastProvider: ToastProvider
+
+    @Inject
+    internal lateinit var navigationProvider: NavigationProvider
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        (context as InjectingDrawerActivity)
+                .component
+                .inject(this)
+
+        beer_rating_overall_column.setOnClickListener { showToast(R.string.description_rating_overall) }
+
+        beer_rating_style_column.setOnClickListener { showToast(R.string.description_rating_style) }
+
+        beer_abv_column.setOnClickListener { showToast(R.string.description_abv) }
+
+        beer_ibu_column.setOnClickListener { showToast(R.string.description_ibu) }
+
+        rating_card_overlay.setOnClickListener { showLoginDialog() }
+    }
+
+    private fun showLoginDialog() {
+        AlertDialog.Builder(context)
+                .setTitle(R.string.login_dialog_title)
+                .setMessage(R.string.login_to_rate_message)
+                .setPositiveButton(R.string.ok) { _, _ -> navigateToLogin() }
+                .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.cancel() }
+                .show()
+    }
+
+    fun setUser(user: User) {
+        ofObj(user).ifSome { rating_card_overlay.visibility = View.GONE }
+    }
+
+    fun setBeer(beer: Beer) {
+        beer_description.text = StringUtils.value(beer.description,
+                resourceProvider.getString(R.string.no_description))
+
+        beer.brewerName?.let { brewer_name.text = it }
+        beer.brewerId?.let { brewerId -> brewer_name_row.setOnClickListener { navigateToBrewer(brewerId) } }
+
+        ofObj(beer.styleId)
+                .map { beerStyleStore.getItem(it) }
+                .orOption { ofObj(beer.styleName).flatMap { beerStyleStore.getStyle(it) } }
+                .ifSome { (id) -> beer_style_row.setOnClickListener { navigateToStyle(id) } }
+                .map { it.name }
+                .orOption { notAvailableString() }
+                .ifSome { beer_style.text = it }
+
+        ofObj(beer.overallRating)
+                .filter { it > 0 }
+                .map { Math.round(it).toString() }
+                .ifSome { beer_rating_overall.text = it }
+                .ifNone {
+                    beer_rating_overall.text = "?"
+                    beer_rating_overall_column.setOnClickListener { showToast(R.string.not_enough_ratings) }
+                }
+
+        ofObj(beer.styleRating)
+                .filter { it > 0 }
+                .map { Math.round(it).toString() }
+                .ifSome { beer_rating_style.text = it }
+                .ifNone {
+                    beer_rating_style.text = "?"
+                    beer_rating_style_column.setOnClickListener { showToast(R.string.not_enough_ratings) }
+                }
+
+        ofObj(beer.alcohol)
+                .filter { it > 0 }
+                .map { String.format(Locale.ROOT, "%.1f%%", it) }
+                .orOption { notAvailableString() }
+                .ifSome { beer_abv.text = it }
+
+        ofObj(beer.ibu)
+                .map { Math.round(it).toString() }
+                .orOption { notAvailableString() }
+                .ifSome { beer_ibu.text = it }
+
+        ofObj(beer.tickValue)
+                .filter { beer.isTicked() }
+                .orOption { ofObj(0) }
+                .ifSome { rating_bar.rating = it.toFloat() }
+
+        ofObj(beer.tickDate)
+                .filter { beer.isTicked() }
+                .map { DateUtils.formatDateTime(resourceProvider.getString(R.string.beer_tick_date), it) }
+                .ifSome { value ->
+                    ticked_date.text = value
+                    ticked_date.visibility = View.VISIBLE
+                }
+                .ifNone { ticked_date.visibility = View.GONE }
+    }
+
+    fun setBrewer(brewer: Brewer) {
+        ofObj(brewer.countryId)
+                .map { countryStore.getItem(it) }
+                .ifSome { (id) -> brewer_country_row.setOnClickListener { navigateToCountry(id) } }
+                .map { String.format("%s, %s", brewer.city, it.name) }
+                .orOption { notAvailableString() }
+                .ifSome { brewer_country.text = it }
+    }
+
+    fun setRatingBarChangeListener(listener: RatingBar.OnRatingBarChangeListener) {
+        rating_bar.onRatingBarChangeListener = listener
+    }
+
+    private fun notAvailableString(): Option<String> {
+        return ofObj(resourceProvider.getString(R.string.not_available))
+    }
+
+    private fun navigateToStyle(styleId: Int) {
+        Timber.d("navigateToStyle(%s)", styleId)
+
+        val intent = Intent(context, StyleDetailsActivity::class.java)
+        intent.putExtra(Constants.ID_KEY, styleId)
+        context.startActivity(intent)
+    }
+
+    private fun navigateToCountry(countryId: Int) {
+        Timber.d("navigateToCountry(%s)", countryId)
+
+        val intent = Intent(context, CountryDetailsActivity::class.java)
+        intent.putExtra(Constants.ID_KEY, countryId)
+        context.startActivity(intent)
+    }
+
+    private fun navigateToBrewer(brewerId: Int) {
+        Timber.d("navigateToBrewer(%s)", brewerId)
+
+        val intent = Intent(context, BrewerDetailsActivity::class.java)
+        intent.putExtra("brewerId", brewerId)
+        context.startActivity(intent)
+    }
+
+    private fun navigateToLogin() {
+        Timber.d("navigateToLogin")
+
+        val intent = Intent(context, ProfileActivity::class.java)
+        context.startActivity(intent)
+    }
+
+    private fun showToast(@StringRes resource: Int) {
+        toastProvider.showCancelableToast(resource, Toast.LENGTH_LONG)
+    }
+}
---
+++ app/src/main/java/quickbeer/android/features/brewerdetails/BrewerDetailsView.kt
@@ -0,0 +1,188 @@
+/**
+ * This file is part of QuickBeer.
+ * Copyright (C) 2017 Antti Poikela <antti.poikela></antti.poikela>@iki.fi>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http:></http:>//www.gnu.org/licenses/>.
+ */
+package quickbeer.android.features.brewerdetails
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.support.annotation.StringRes
+import android.support.v4.widget.NestedScrollView
+import android.util.AttributeSet
+import android.widget.Toast
+import kotlinx.android.synthetic.main.brewer_details_fragment_details.view.*
+import polanski.option.Option
+import polanski.option.Option.ofObj
+import quickbeer.android.Constants
+import quickbeer.android.R
+import quickbeer.android.analytics.Analytics
+import quickbeer.android.analytics.Events.LaunchAction
+import quickbeer.android.core.activity.InjectingDrawerActivity
+import quickbeer.android.data.pojos.Brewer
+import quickbeer.android.data.stores.CountryStore
+import quickbeer.android.features.countrydetails.CountryDetailsActivity
+import quickbeer.android.providers.ResourceProvider
+import quickbeer.android.providers.ToastProvider
+import quickbeer.android.utils.StringUtils
+import quickbeer.android.utils.StringUtils.emptyAsNone
+import timber.log.Timber
+import javax.inject.Inject
+
+/**
+ * View holder for all the brewer details
+ */
+class BrewerDetailsView(context: Context, attrs: AttributeSet) : NestedScrollView(context, attrs) {
+
+    private val VISIBLE = 1.0f
+    private val OPAQUE = 0.2f
+
+    @Inject
+    internal lateinit var countryStore: CountryStore
+
+    @Inject
+    internal lateinit var resourceProvider: ResourceProvider
+
+    @Inject
+    internal lateinit var toastProvider: ToastProvider
+
+    @Inject
+    internal lateinit var analytics: Analytics
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        (context as InjectingDrawerActivity)
+                .component
+                .inject(this)
+    }
+
+    fun setBrewer(brewer: Brewer) {
+        ofObj(brewer.founded)
+                .map { it.year.toString() }
+                .orOption { notAvailableOption() }
+                .ifSome { brewer_founded.text = it }
+
+        ofObj(brewer.website)
+                .filter { StringUtils.hasValue(it) }
+                .map { StringUtils.removeTrailingSlash(it) }
+                .map { StringUtils.addMissingProtocol(it) }
+                .ifSome { website ->
+                    brewer_website.alpha = VISIBLE
+                    brewer_website_column.setOnClickListener { openUri(LaunchAction.BREWER_WEBSITE, website) }
+                }
+                .ifNone {
+                    brewer_website.alpha = OPAQUE
+                    brewer_website_column.setOnClickListener { showToast(R.string.brewer_details_no_website) }
+                }
+
+        ofObj(brewer.facebook)
+                .filter { StringUtils.hasValue(it) }
+                .map { String.format(Constants.FACEBOOK_PATH, it) }
+                .ifSome { facebook ->
+                    brewer_facebook.alpha = VISIBLE
+                    brewer_facebook_column.setOnClickListener { openUri(LaunchAction.BREWER_FACEBOOK, facebook) }
+                }
+                .ifNone {
+                    brewer_facebook.alpha = OPAQUE
+                    brewer_facebook_column.setOnClickListener { showToast(R.string.brewer_details_no_facebook) }
+                }
+
+        ofObj(brewer.twitter)
+                .filter { StringUtils.hasValue(it) }
+                .map { String.format(Constants.TWITTER_PATH, it) }
+                .ifSome { twitter ->
+                    brewer_twitter.alpha = VISIBLE
+                    brewer_twitter_column.setOnClickListener { openUri(LaunchAction.BREWER_TWITTER, twitter) }
+                }
+                .ifNone {
+                    brewer_twitter.alpha = OPAQUE
+                    brewer_twitter_column.setOnClickListener { showToast(R.string.brewer_details_no_twitter) }
+                }
+
+        ofObj(brewer.countryId)
+                .map { countryStore.getItem(it) }
+                .ifSome { (id) -> brewer_country_row.setOnClickListener { navigateToCountry(id) } }
+                .map { it.name }
+                .orOption { notAvailableOption() }
+                .ifSome { brewer_country.text = it }
+
+        ofObj(brewer.city)
+                .filter { StringUtils.hasValue(it) }
+                .ifSome { city -> brewer_city_row.setOnClickListener { openWikipedia(city) } }
+                .orOption { notAvailableOption() }
+                .ifSome { brewer_city.text = it }
+
+        ofObj(brewer.address)
+                .filter { StringUtils.hasValue(it) }
+                .orOption { notAvailableOption() }
+                .ifSome { brewer_address.text = it }
+
+        fullAddress(brewer)
+                .ifSome { address -> brewer_address_row.setOnClickListener { openMaps(address) } }
+    }
+
+    private fun notAvailableOption(): Option<String> {
+        return ofObj(notAvailableString())
+    }
+
+    private fun notAvailableString(): String {
+        return resourceProvider.getString(R.string.not_available)
+    }
+
+    private fun openUri(action: LaunchAction, uri: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+        context.startActivity(intent)
+
+        analytics.createEvent(action)
+    }
+
+    private fun navigateToCountry(countryId: Int) {
+        Timber.d("navigateToCountry(%s)", countryId)
+
+        val intent = Intent(context, CountryDetailsActivity::class.java)
+        intent.putExtra(Constants.ID_KEY, countryId)
+        context.startActivity(intent)
+    }
+
+    private fun openWikipedia(article: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(String.format(Constants.WIKIPEDIA_PATH, article)))
+        context.startActivity(intent)
+    }
+
+    private fun openMaps(address: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(String.format(Constants.GOOGLE_MAPS_PATH, address)))
+        context.startActivity(intent)
+    }
+
+    private fun fullAddress(brewer: Brewer): Option<String> {
+        return ofObj(brewer.countryId)
+                .map { countryStore.getItem(it) }
+                .map { it.name }
+                .lift(emptyAsNone(brewer.city), emptyAsNone(brewer.address)) { country, city, address ->
+                    val street = if (address.contains(","))
+                        address.split(",")[0]
+                    else
+                        address
+
+                    String.format("%s, %s, %s", street, city, country)
+                }
+    }
+
+    private fun showToast(@StringRes resource: Int) {
+        toastProvider.showCancelableToast(resource, Toast.LENGTH_LONG)
+    }
+}
