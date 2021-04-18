d128e97237f64d9f3b1ba15af61e231e2418065c
Antti Poikela <antti.poikela@iki.fi>
2021-04-19 00:54:59 +0200

    LeakCanary logging can't be defined in release mode

--- app/src/main/java/quickbeer/android/MainApplication.kt
+++ app/src/main/java/quickbeer/android/MainApplication.kt
@@ -8,7 +8,6 @@ import com.jakewharton.threetenabp.AndroidThreeTen
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import quickbeer.android.inject.appModule
-import shark.SharkLog
 import timber.log.Timber
 
 class MainApplication : Application(), ImageLoaderFactory {
@@ -19,7 +18,6 @@ class MainApplication : Application(), ImageLoaderFactory {
         initThreeTenAbp()
         initKoin()
         initTimber()
-        initLeakCanary()
 
         Timber.d("App running!")
     }
@@ -38,13 +36,6 @@ class MainApplication : Application(), ImageLoaderFactory {
     private fun initTimber() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
-        }
-    }
-
-    private fun initLeakCanary() {
-        SharkLog.logger = object : SharkLog.Logger {
-            override fun d(message: String) = Unit
-            override fun d(throwable: Throwable, message: String) = Unit
         }
     }
 
