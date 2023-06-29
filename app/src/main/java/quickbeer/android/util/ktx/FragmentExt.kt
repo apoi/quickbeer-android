3374b6552c4b95c90e8155e88f9135f14e8f4ba2
Antti Poikela <antti.poikela@iki.fi>
2023-06-29 14:45:54 +0200

    Support list ref releasing in fragment binding

--- app/src/main/java/quickbeer/android/util/ktx/FragmentExt.kt
+++ app/src/main/java/quickbeer/android/util/ktx/FragmentExt.kt
@@ -2,20 +2,27 @@ package quickbeer.android.util.ktx
 
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import quickbeer.android.data.state.State
 
 /**
  * Flow observing helper.
  */
 fun <T> Fragment.observe(flow: Flow<T>, observer: (T) -> Unit) {
-    viewLifecycleOwner.lifecycle.coroutineScope.launchWhenStarted {
-        flow.collectLatest { observer.invoke(it) }
+    viewLifecycleOwner.lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest { event -> observer(event) }
+        }
     }
 }
 
@@ -33,15 +40,38 @@ fun Fragment.hideKeyboard() {
  * Keep ViewBinding in View tag for convenient lifecycle management.
  */
 inline fun <reified T : ViewBinding> Fragment.viewBinding(
-    crossinline bind: (View) -> T
+    crossinline bind: (View) -> T,
+    noinline destroyCallback: ((T) -> Unit)? = null
 ): ReadOnlyProperty<Fragment, T> = object : ReadOnlyProperty<Fragment, T> {
 
-    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
-        val value = requireView().getTag(property.name.hashCode()) as? T
-        if (value != null) return value
+    private var binding: T? = null
 
-        return bind(requireView()).also {
-            requireView().setTag(property.name.hashCode(), it)
+    init {
+        viewLifecycleOwnerLiveData.observe(this@viewBinding) { viewLifecycleOwner ->
+            viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+                override fun onDestroy(owner: LifecycleOwner) {
+                    binding?.let { destroyCallback?.invoke(it) }
+                    binding = null
+                }
+            })
+        }
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        binding?.let { return it }
+
+        val viewLifecycleOwner = try {
+            thisRef.viewLifecycleOwner
+        } catch (e: IllegalStateException) {
+            error("Fragment view hasn't been created")
+        }
+
+        if (!viewLifecycleOwner.lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
+            error("Attempt to access binding after Fragment view was destroyed")
+        }
+
+        return bind(thisRef.requireView()).also { viewBinding ->
+            binding = viewBinding
         }
     }
 }
