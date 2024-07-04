19a871852eaabe487040bce7dcd141ccc34939af
Antti Poikela <antti.poikela@iki.fi>
2024-07-04 19:09:15 +0200

    Add basic actions menu dialog

--- app/src/main/java/quickbeer/android/ui/base/BaseBottomSheetFragment.kt
+++ app/src/main/java/quickbeer/android/ui/base/BaseBottomSheetFragment.kt
@@ -1,5 +1,13 @@
 package quickbeer.android.ui.base
 
+import android.os.Bundle
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import quickbeer.android.R
 
-abstract class BaseBottomSheetFragment : BottomSheetDialogFragment()
+abstract class BaseBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.AppTheme_BottomSheet)
+    }
+}
