ffb383d41213da748fc3962575ac4a8474173242
Antti Poikela <antti.poikela@iki.fi>
2024-08-03 16:21:47 +0200

    Add back missing paragraph handler in adapter

--- app/src/main/java/quickbeer/android/network/adapter/StripHtmlStringAdapter.kt
+++ app/src/main/java/quickbeer/android/network/adapter/StripHtmlStringAdapter.kt
@@ -3,11 +3,18 @@ package quickbeer.android.network.adapter
 import androidx.core.text.HtmlCompat
 import androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY
 import com.squareup.moshi.FromJson
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class StripHtmlStringAdapter {
 
     @FromJson
     fun deserialize(json: String): String {
+        val value = PATTERN.matcher(json).replaceAll(Matcher.quoteReplacement("<p>"))
         return HtmlCompat.fromHtml(json, FROM_HTML_MODE_LEGACY).toString().trim { it <= ' ' }
+    }
+
+    companion object {
+        private val PATTERN = Pattern.compile("<br ?/?>|\n", Pattern.CASE_INSENSITIVE)
     }
 }
